6df59cc7874beb6d550b4b62c1b0c2f9aef1c7a2
TheAkio <me@theak.io>
2017-07-28 11:52:08 +0200

    Added all stuff

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Set up winston
+const winston = require('winston');
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, {
+    timestamp: true,
+    colorize: true,
+});
+
+// Middleware
+module.exports.BaseMiddleware = require('./lib/middleware/base.middleware');
+
+// Routers
+module.exports.BaseRouter = require('./lib/routers/base.router');
+module.exports.GenericRouter = require('./lib/routers/generic.router');
+module.exports.WildcardRouter = require('./lib/routers/wildcard.router');
+
+// Other stuff
+module.exports.Constants = require('./lib/constants');
