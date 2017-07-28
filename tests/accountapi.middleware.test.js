471511f1769bb46054066b8a8734265f0655627c
TheAkio <me@theak.io>
2017-07-28 17:58:01 +0200

    Reworked test system

--- tests/accountapi.middleware.test.js
+++ tests/accountapi.middleware.test.js
@@ -1,41 +1,55 @@
 'use strict';
 
+const BaseTest = require('./base.test');
+
 const express = require('express');
 const superagent = require('superagent');
 const AccountAPIMiddleware = require('../lib/middleware/accountapi.middleware');
 
-const token = 'test_token';
+class AccountAPIMiddlewareTest extends BaseTest {
+    constructor() {
+        super('Account API Middleware');
+    }
 
-let accountApi = express();
-let apiRouter = express.Router();
-apiRouter.get('/validate/:token', (req, res) => {
-    if (req.params.token !== token) return res.status(401).json({ status: 401, message: 'Unauthorized' });
-    return res.status(200).json({
-        account: {
-            name: 'test',
-        },
-    });
-});
-accountApi.use(apiRouter);
-let apiServer = accountApi.listen(12010, 'localhost', () => {
-    let app = express();
-    app.use(new AccountAPIMiddleware('http://localhost:12010').middleware());
-    app.use('*', (req, res) => res.status(200).json({ status: 200, message: 'Authorized', account: req.account }));
-    let server = app.listen(12011, 'localhost', () => {
-        superagent
-        .get(`http://localhost:12011`)
-        .set('Content-Type', 'application/json')
-        .set('Authorization', `Bearer ${token}`)
-        .send()
-        .then(response => {
-            console.log(response.body);
-            apiServer.close();
-            server.close();
-        })
-        .catch(e => {
-            console.log(e);
-            apiServer.close();
-            server.close();
+    run() {
+        const token = 'test_token';
+        return new Promise((resolve, reject) => {
+            let accountApi = express();
+            let apiRouter = express.Router();
+            apiRouter.get('/validate/:token', (req, res) => {
+                if (req.params.token !== token) return res.status(401).json({ status: 401, message: 'Unauthorized' });
+                return res.status(200).json({
+                    account: {
+                        name: 'test',
+                    },
+                });
+            });
+            accountApi.use(apiRouter);
+            let apiServer = accountApi.listen(12010, 'localhost', () => {
+                let app = express();
+                app.use(new AccountAPIMiddleware('http://localhost:12010').middleware());
+                app.use('*', (req, res) => res.status(200).json({ status: 200, message: 'Authorized', account: req.account }));
+                let server = app.listen(12011, 'localhost', () => {
+                    superagent
+                    .get(`http://localhost:12011`)
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', `Bearer ${token}`)
+                    .send()
+                    .then(response => {
+                        apiServer.close();
+                        server.close();
+                        if (response.body && response.body.status === 200 && response.body.message === 'Authorized' && response.body.account) resolve();
+                        reject(Error('Unexpected response or no account'));
+                    })
+                    .catch(e => {
+                        apiServer.close();
+                        server.close();
+                        reject(e);
+                    });
+                });
+            });
         });
-    });
-});
+    }
+}
+
+module.exports = AccountAPIMiddlewareTest;
