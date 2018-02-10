43399a54d6d7475adbfd8d5ffddd1d4b30a18878
daswolke <wolke@weeb.sh>
2018-02-10 13:06:43 +0100

    apply js-standard and update dependencies

--- test.js
+++ test.js
@@ -1,49 +1,48 @@
-'use strict';
-
 // Set up winston
-const winston = require('winston');
-winston.remove(winston.transports.Console);
+const winston = require('winston')
+winston.remove(winston.transports.Console)
 winston.add(winston.transports.Console, {
-    timestamp: true,
-    colorize: true,
-});
+  timestamp: true,
+  colorize: true,
+})
 
 try {
-    const pkg = require('./package.json');
-    const fs = require('fs');
+  const pkg = require('./package.json')
+  const fs = require('fs')
 
-    let files = fs.readdirSync('./tests');
-    let tests = [];
-    for (let i = 0; i < files.length; i++) {
-        if (files[i] !== 'base.test.js') tests.push(require(`./tests/${files[i]}`));
+  const files = fs.readdirSync('./tests')
+  const tests = []
+  for (let i = 0; i < files.length; i++) {
+    if (files[i] !== 'base.test.js') tests.push(require(`./tests/${files[i]}`))
+  }
+  winston.info('Tests loaded.')
+
+  winston.info(`Testing wapi-core@${pkg.version}`)
+
+  let successful = 0
+  let failed = 0
+
+  const runTests = async () => {
+    for (let i = 0; i < tests.length; i++) {
+      const test = new tests[i]()
+      winston.info(`Running test for ${test.name}`)
+      try {
+        await test.run()
+        successful++
+      } catch (e) {
+        failed++
+        return winston.error(`Test failed: ${e}`)
+      }
     }
-    winston.info('Tests loaded.');
+  }
 
-    winston.info(`Testing wapi-core@${pkg.version}`);
-
-    let successful = 0, failed = 0;
-
-    let runTests = async() => {
-        for (let i = 0; i < tests.length; i++) {
-            let test = new tests[i];
-            winston.info(`Running test for ${test.name}`);
-            try {
-                await test.run();
-                successful++;
-            } catch (e) {
-                failed++;
-                return winston.error(`Test failed: ${e}`);
-            }
-        }
-    };
-
-    runTests()
+  runTests()
     .then(() => {
-        winston.info(`Done. Completed ${successful} test(s) successfully, failed ${failed} test(s).`);
+      winston.info(`Done. Completed ${successful} test(s) successfully, failed ${failed} test(s).`)
     })
     .catch(e => {
-        winston.error(`Failed to run tests: ${e}`);
-    });
+      winston.error(`Failed to run tests: ${e}`)
+    })
 } catch (e) {
-    winston.error(`Failed to run tests: ${e}`);
+  winston.error(`Failed to run tests: ${e}`)
 }
