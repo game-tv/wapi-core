052180c943aa49938059be67c1722201cda2402e
daswolke <wolke@weeb.sh>
2018-03-12 21:20:22 +0100

    update exports & dependencies

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 'use strict'
 
-// Set up winston
-const winston = require('winston')
-winston.remove(winston.transports.Console)
-winston.add(winston.transports.Console, {
-  timestamp: true,
-  colorize: true
-})
-
 // Middleware
 module.exports.BaseMiddleware = require('./lib/middleware/base.middleware')
 module.exports.AccountAPIMiddleware = require('./lib/middleware/accountapi.middleware')
@@ -22,3 +14,8 @@ module.exports.WildcardRouter = require('./lib/routers/wildcard.router')
 // Other stuff
 module.exports.Constants = require('./lib/constants')
 module.exports.Utils = require('./lib/utils')
+module.exports.Logger = require('./lib/logger')
+module.exports.Registrator = require('./lib/registrator')
+module.exports.Errors = {
+  HttpError: require('./lib/structures/errors/HttpError'),
+}
